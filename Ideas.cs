2ca5f0f61471d2c21275bb994c3344aaa301a28d
leumas-iahim <samtoma0900@gmail.com>
2024-10-07 18:59:48 -0400

    Add files via upload

---
+++ Ideas.cs
@@ -0,0 +1,20 @@
+
+
+
+
+// A boy (Kris Dreemurr) is sent to his room by his mom (Toriel Dreemurr) because he was trying to calm down an argument between his father (Asgore Dreemurr) and his mother, but after being sent to his
+// room, the argument intensifies, and the boy blames himself for it. Ever since the boy's brother (Asriel Dreemurr) left for college, things between the mother and the father just keep getting worse and
+// worse. Asriel was always the one who was able to calm them down, but now that he left to go to college, the responsibility has fallen onto Kris's shoulders and he just can't take it anymore. So, he
+// packed some of his things and ran from home and in to the forest as it began to rain. After a little while of walking in the woods, Kris found a pair of push open doors on the side of a mound hanging
+// loosely open, he thought of going inside to get out of the rain, but decides not to go inside because it is dark feels unsettling and he is already fully drenched from the rain. But after seeing a
+// light and hearing something that was almost as if it was calling for him, despite his better judgment, he decides to go walk inside. Unfortunately, the whole room was in fact a hole. He steps inside and  so on
+// he instantly falls into a massive void. After fallin for a bit (roughly 30 seconds gameplay wise), Kris falls through something like glass made of darkness cutting his clothes and body.
+// After  this, Kris's fall slows down until he stops on a floor. The floor was made of pure darkness face on the ground. A disembodied voice then starts to talk to Kris and telling him to get up, so
+// Kris gets up and the voice starts to talk about Kris's life and makes an offer to Kris saying "I have a deal to offer you Kris, the offer is I give you fame, power, importance, love, fun, adventure, thrill,
+// glory and charisma. In exchange, you have to thake that shard of darkness beside you, relinquish control of your body, and give up your soul."
+// Due to the offering, that was almost all what Kris wanted deeplly inside, he didn't even hear the price he must pay, so he accepted the deal and cuts out his soul.
+// - Now the player is getting control over Kris's body.
+
+// Along with the memories of the original Kris from Deltarune (all the way up to chapter two) coming in the form of vivid dreams when he sleeps. When he wake's up, he is in the underground from
+// Undertale, but the monsters colors are inverted with semi-opposite personalities to their original counterparts. On top of all this, the underground is flipped vertically and horizontally and at
+// certain points you will have to flip from the floor to the celing (or flip back to the floor) of the underground to get to certain places and to continue the game.
